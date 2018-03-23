382b998c32207f31c3107607a656223ee882bb87
enerdgy <37632621+enerdgy@users.noreply.github.com>
2018-03-23 08:48:50 -0700

    Update GCE.bat

--- GCE.bat
+++ GCE.bat
@@ -9,10 +9,12 @@
 	    ::ex1 - set chrome=c:\temp\googlechromestandaloneenterprise64.msi
         ::ex2 - set chrome=\\server\share\googlechromestandaloneenterprise64.msi
     set Chrome=
-    ::The next line checks to see if Chrome variable is set and exits if its null
-    IF %Chrome% == "" GOTO :EOF
-    set RegExt=0
-    set RegDef=1
+    ::The next line checks to see if Chrome variable is set and exits if its empty
+    	IF %Chrome% == "" GOTO :EOF
+    ::Set to 1 if you want to Disable all extensions
+    	set RegExt=0
+    ::set to 1 if you want Chrome to set its self as default browser.
+    	set RegDef=1
 
 ::set working Drive and working directory
     C:
@@ -26,12 +28,12 @@
 
 ::Add Registry keys
 	::Disable all extentions on Computer if regext=1
-		if "%regext%" == "1" (
+		if "%RegExt%" == "1" (
             REG ADD HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallBlacklist /v "1" /t REG_SZ /d "*" /f
         )
 	
 	::Force Google Chrome as default Browser
-		if "%regext%" == "1" (
+		if "%RegDef%" == "1" (
             REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v "DefaultBrowserSettingEnabled" /t REG_DWORD /d "1" /f
         )
 EXIT
